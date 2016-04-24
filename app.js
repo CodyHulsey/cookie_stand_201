b78949f1aca92e672aebea26c49238d3497b2265
CodyHulsey <sendcodyamessage@live.com>
2016-04-23 19:09:50 -0700

    I added a render function to my code.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var hours = ['10AM:', '11AM:', '12AM:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:',];
 
-var demand = [];
-
 var pikePlace = {
   name: 'Pike Place',
   min: 17,
@@ -16,11 +14,19 @@ var pikePlace = {
     for (var i = 0; i < hours.length; i++) {
       this.hourlySales.push(Math.floor(this.avgSales * this.customerPerHour(this.min, this.max)));
       this.totalSales = this.totalSales + this.hourlySales[i];
+}
+},
+  render: function() {
+    var ulEl = document.createElement('ul');
+    var keys = Object.keys(this);
+    for (var j = 0; j < 5; j++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = this[keys[j]];
+      ulEl.appendChild(liEl);
+      }
+    sectionEl.appendChild(ulEl);
   }
 }
-}
-
-// customerPerHour(minCustomers, maxCustomers);
 
 var sectionEl = document.getElementById('pike-Place');
 var ulEl = document.createElement('ul');
@@ -29,14 +35,12 @@ for(hour in hours) {
   var liEl = document.createElement('li');
   liEl.textContent = hours[hour];
   var nestedUlEl = document.createElement('ul');
-
-  for(value in demand) {
-    var nestedLiEl = document.createElement('li');
-    nestedLiEl.textContent = demand[value];
-    nestedUlEl.appendChild(nestedLiEl);
-  }
+  // for(value in demand) {
+  //   var nestedLiEl = document.createElement('li');
+  //   nestedLiEl.textContent = demand[value];
+  //   nestedUlEl.appendChild(nestedLiEl);
   liEl.appendChild(nestedUlEl);
   ulEl.appendChild(liEl);
-  }
+}
 
 sectionEl.appendChild(ulEl);
