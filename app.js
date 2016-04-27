08df1de462dc67ab7fd01c20917098f30680b934
CodyHulsey <sendcodyamessage@live.com>
2016-04-26 18:16:52 -0700

    The cookie sales by hour are not appending to the page.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var hours = ['10AM:', '11AM:', '12AM:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:',];
+var hours = ['10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM',];
 
 var pikePlace = {
   name: 'Pike Place',
@@ -13,53 +13,31 @@ var pikePlace = {
 },
   generateHourly: function(arr, totals, avg, rand, hour) {
     for(var j = 0; j < hour.length; j++) {
-        var ran = Math.floor(rand() * avg);
+        var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
         arr.push(ran);
-        totals += ran;
+        this.totals += ran;
         }
 },
-
   render: function() {
-    this.generateHourly();
-
+    this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
     var ulEl = document.createElement('ul');
     ulEl.appendChild(document.createTextNode(this.name));
     var sectionEl = document.getElementById('pikePlace').appendChild(ulEl);
 
-    for (var i = 0; i < hours.length; i++) {
+    for(var i = 0; i < hours.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = hours[i] + ": " + this.hourlySales[i];
       ulEl.appendChild(liEl);
     }
-
     var liEl_two = document.createElement('li');
     liEl_two.textContent = 'Total: ' + this.totals;
     ulEl.appendChild(liEl_two);
   }
 }
-// var sectionEl = document.getElementById('pike-Place');
-// var ulEl = document.createElement('ul');
-//
-// for(hour in hours) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = hours[hour];
-//   var nestedUlEl = document.createElement('ul');
-//   // for(value in demand) {
-//   //   var nestedLiEl = document.createElement('li');
-//   //   nestedLiEl.textContent = demand[value];
-//   //   nestedUlEl.appendChild(nestedLiEl);
-//   liEl.appendChild(nestedUlEl);
-//   ulEl.appendChild(liEl);
-// }
-//
-// sectionEl.appendChild(ulEl);
 
-//var stores = [
-//   pikeStore,
-//   alkiStore,
-//   seaTacStore,
-//   bellevueSqStore,
-//   southCenterStore,
-// ];
-//for(var k = 0; k < stores.length; k++)
-//stores.render(k);
+var stores = [
+  pikePlace,
+];
+for (var k = 0; k < stores.length; k++) {
+  stores[k].render();
+}
