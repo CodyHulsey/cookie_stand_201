e8c16edb38fc488a1dd5b1cbf0034939772f2cc3
CodyHulsey <sendcodyamessage@live.com>
2016-04-26 19:20:37 -0700

    Added the stores to js and made it so that they are appending to the html.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var hours = ['10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM',];
 
-var pikePlace = {
+var stores = [
+{
   name: 'Pike Place',
   min: 17,
   max: 88,
@@ -33,11 +34,140 @@ var pikePlace = {
     liEl_two.textContent = 'Total: ' + this.totals;
     ulEl.appendChild(liEl_two);
   }
-}
+},
+{
+  name: 'SeaTac Airport',
+  min: 6,
+  max: 24,
+  avgSales: 1.2,
+  hourlySales: [],
+  totals: 0,
+  customerPerHour: function () {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
+  generateHourly: function(arr, totals, avg, rand, hour) {
+    for(var j = 0; j < hours.length; j++) {
+      var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
+      arr.push(ran);
+      this.totals += ran;
+    }
+  },
+  render: function() {
+    this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var sectionEl = document.getElementById('seaTacAirport').appendChild(ulEl);
 
-var stores = [
-  pikePlace,
-];
+    for(var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.hourlySales[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl_two = document.createElement('li');
+    liEl_two.textContent = 'Total: ' + this.totals;
+    ulEl.appendChild(liEl_two);
+  }
+},
+{
+  name: 'Southcenter',
+  min: 11,
+  max: 38,
+  avgSales: 1.9,
+  hourlySales: [],
+  totals: 0,
+  customerPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
+  generateHourly: function(arr, totals, avg, rand, hour) {
+    for(var j = 0; j < hours.length; j++) {
+      var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
+      arr.push(ran);
+      this.totals += ran;
+    }
+  },
+  render: function() {
+    this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var sectionEl = document.getElementById('southCenter').appendChild(ulEl);
+
+    for(var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.hourlySales[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl_two = document.createElement('li');
+    liEl_two.textContent ='Total: ' + this.totals;
+    ulEl.appendChild(liEl_two);
+    }
+  },
+{
+  name: 'Bellevue Square',
+  min: 20,
+  max: 48,
+  avgSales: 3.3,
+  hourlySales: [],
+  totals: 0,
+  customerPerHour: function () {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
+  generateHourly: function(arr, totals, avg, rand, hour) {
+    for(var j = 0; j < hours.length; j++) {
+      var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
+      arr.push(ran);
+      this.totals += ran;
+    }
+  },
+  render: function() {
+    this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var sectionEl = document.getElementById("bellevueSquare").appendChild(ulEl);
+
+    for(var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.hourlySales[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl_two = document.createElement('li');
+    liEl_two.textContent = 'Total: ' + this.totals;
+    ulEl.appendChild(liEl_two);
+  }
+},
+{
+  name: "Alki",
+  min: 3,
+  max: 24,
+  avgSales: 2.6,
+  hourlySales: [],
+  totals: 0,
+  customerPerHour: function () {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
+  generateHourly: function (arr, totals, avg, rand, hour) {
+    for(var j = 0; j < hours.length; j++) {
+      var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
+      arr.push(ran);
+      this.totals += ran;
+    }
+  },
+  render: function() {
+    this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var sectionEl = document.getElementById('alki').appendChild(ulEl);
+
+    for(var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.hourlySales[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl_two = document.createElement('li');
+    liEl_two.textContent = 'Total: ' + this.totals;
+    ulEl.appendChild(liEl_two);
+    }
+  },
+]
 for (var k = 0; k < stores.length; k++) {
   stores[k].render();
 }
