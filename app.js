a40680fcb12ab93e5058922d5f708484663d78aa
CodyHulsey <sendcodyamessage@live.com>
2016-05-05 19:42:19 -0700

    I figured out how to structure my table, and allow users to add their own shops.

--- app.js
+++ app.js
@@ -1,47 +1,17 @@
-// var submit = document.getElementById('test');
-//
-// function foo() {
-//   // console.log('hello word');
-//   console.log(this);
-//   // this.removeEventListener('click', foo);
-// }
-// // submit.addEventListener('click', foo);
-//
-// var formEl = document.getElementById('myForm');
-// var inputSN = document.getElementById('storeName');
-// var inputmin = document.getElementById('min');
-// var inputmax = document.getElementById('max');
-// var tableEl = document.getElementById('myForm');
-//
+var hours = ['10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM',];
 
-// function getInputs() {
-//   event.preventDefault();
-//   console.log(event.target);
-// }
+var pikePlace = new Store('Pike Place', 17, 88, 5.2);
+var seaTacAirport = new Store('SeaTac Airport', 6, 24, 1.2);
+var Southcenter = new Store('Southcenter', 11, 38, 1.9);
+var bellevueSquare = new Store('Bellevue Square', 20, 48, 3.3);
+var alki = new Store('Alki', 3, 24, 2.6);
 
-myForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  // console.log(event.target[1].value);
-  console.log(typeof inputAge.value);
-  var trEl = document.createElement('tr');
-  var tdOne = document.createElement('td');
-  var tdTwo = document.createElement('td');
-  var tdThree = document.createElement('td');
-  tdOne.textContent = inputFN.value;
-  tdTwo.textContent = inputLN.value;
-  tdThree.textContent = inputAge.value;
-  trEl.appendChild(tdOne);
-  trEl.appendChild(tdTwo);
-  trEl.appendChild(tdThree);
-  tableEl.appendChild(trEl);
-});
-
-function Store(name, min, max, avgSales, hourlySales, totals, customerPerHour, generateHourly) {
+function Store(name, min, max, avgSales) {
   this.name = name;
   this.min = min;
   this.max = max;
   this.avgSales = avgSales;
-  this.hourlySales = hourlySales;
+  this.hourlySales = [];
   this.totals = 0;
 }
 
@@ -50,49 +20,94 @@ Store.prototype.customerPerHour = function(min, max) {
 };
 
 Store.prototype.generateHourly = function(arr, totals, avg, ran, hour) {
-  for(var j = 0; j < hour.length; j++) {
-    var ran = Math.floor(this.customerPerHour(this.max, this.min) * avg);
-    arr.push(ran);
+  for(var j = 0; j < hours.length; j++) {
+    var ran = Math.floor(this.customerPerHour(this.max, this.min) * this.avgSales);
+    this.hourlySales.push(ran);
     this.totals += ran;
   }
 };
+var tableEl = document.getElementById('stores');
+// var newStore = document.createElement('tr');
+var rowEl = document.createElement('tr');
+var emptyCell = document.createElement('th');
+var totalsTitle = document.createElement('th');
 
-// (Store.renderTable = function() {
-//
-// }
+emptyCell.textContent = '';
+totalsTitle.textContent = 'Totals';
 
+tableEl.appendChild(rowEl);
+rowEl.appendChild(emptyCell);
+
+for(var i = 0; i < hours.length; i++) {
+  var tHours = document.createElement('th');
+  tHours.textContent = hours[i];
+  rowEl.appendChild(tHours);
+}
+rowEl.appendChild(totalsTitle);
 Store.prototype.render = function() {
   this.generateHourly(this.hourlySales, this.totals, this.avgSales, this.customerPerHour, hours);
-  var tableEl = document.getElementById('stores');
-  var newStore = document.createElement('th');
-  tableEl.appendChild(newStore);
-  var ulEl = document.createElement('th');
+  //
+  // Store.prototype.render = function() {
+  //   this.generateHourly();
+    var dataRows = document.createElement('tr');
+    var tdStoreNames = document.createElement('tr');
+    tdStoreNames.appendChild(document.createTextNode(this.name));
+    dataRows.appendChild(tdStoreNames);
+    tableEl.appendChild(dataRows);
 
-  for(var i = 0; i < hours.length; i++) {
-    var liEl = document.createElement('th');
-    liEl.textContent = hours[i] + ': ' + this.hourlySales[i];
-    ulEl.appendChild(liEl);
-  }
-  var liEl_two = document.createElement('th');
-  liEl_two.textContent = 'Total: ' + this.totals;
-  ulEl.appendChild(liEl_two);
-  newStore.textContent = this.name;
-  newStore.appendChild(ulEl);
-};
-
-var pikePlace = new Store('Pike Place', 17, 88, 5.2, []);
-var seaTacAirport = new Store('SeaTac Airport', 6, 24, 1.2, []);
-var Southcenter = new Store('Southcenter', 11, 38, 1.9, []);
-var bellevueSquare = new Store('Bellevue Square', 20, 48, 3.3, []);
-var alki = new Store('Alki', 3, 24, 2.6, []);
-
-var hours = ['10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM',];
-
-// var tbl;
-
+    for(var i = 0; i < hours.length; i++) {
+      var dataHours = document.createElement('td');
+      dataHours.textContent = this.hourlySales[i];
+      dataRows.appendChild(dataHours);
+    }
+    var dataTotals = document.createElement('td');
+    dataTotals.textContent = this.totals;
+    dataRows.appendChild(dataTotals);
+  };
 
 pikePlace.render();
 seaTacAirport.render();
 Southcenter.render();
 bellevueSquare.render();
 alki.render();
+
+Store.renderNew = function(obj) {
+  var newRow = document.createElement('tr');
+  var nameTd = document.createElement('td');
+  nameTd.textContent = obj.name;
+  newRow.appendChild(nameTd);
+
+  for(hour in hours) {
+    var salesTd = document.createElement('td');
+    salesTd.textContent = obj.hourlySales[hour];
+    newRow.appendChild(salesTd);
+  }
+  var newTotalTd = document.createElement('td');
+  newTotalTd.textContent = obj.totals;
+  newRow.appendChild(newTotalTd);
+  tableEl.appendChild(newRow);
+};
+
+var formEl = document.getElementById('myForm');
+
+myForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var newStoreName = event.target.storeName.value;
+  var newMinCust = parseInt(event.target.min.value);
+  var newMaxCust = parseInt(event.target.max.value);
+  var newAvgCustSale = parseFloat(event.target.avg.value);
+  // console.log(newStoreName);
+  var newShop = new Store(newStoreName, newMinCust, newMaxCust, newAvgCustSale);
+  newShop.generateHourly();
+
+  Store.renderNew(newShop);
+
+  var inputSN = document.getElementById('storeName');
+  var inputMin = document.getElementById('min');
+  var inputMax = document.getElementById('max');
+  var inputAvg = document.getElementById('avg');
+  var tableEl = document.getElementById('myForm');
+  var insertNewStore = new Store(newStoreName, newMinCust, newMaxCust, newAvgCustSale);
+
+  newStoreName.textContent = event.target.newstorelocation.value;
+});
